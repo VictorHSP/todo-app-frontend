cb3d4b9375fd46fa5054946148c8f9566480a64d
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-22 14:53:53 -0200

    Criação do componente PageHeader e dos componentes formulário e lista para Todo.

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name={'Sobre'} small={'nós'}/>
+
+        <div className='row text-center '>
+            <div className='col-md-4'>
+                <h2>Nossa História</h2>
+                <p>
+                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos,
+                    e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
+                    de tipos e os embaralhou para fazer um livro de modelos de tipos
+                </p>
+            </div>
+
+            <div className='col-md-4'>
+                <h2>Sobre Nós</h2>
+                <p>
+                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos,
+                    e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
+                    de tipos e os embaralhou para fazer um livro de modelos de tipos
+                </p>
+            </div>
+
+            <div className='col-md-4'>
+                <h2>Carreira</h2>
+                <p>
+                    Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos,
+                    e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
+                    de tipos e os embaralhou para fazer um livro de modelos de tipos
+                </p>
+            </div>
+
+        </div>
+
+
     </div>
 )
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
 
 export default class Todo extends Component {
 
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <PageHeader name={'Tarefas'} small={'Cadastro'} />
+                <TodoForm />
             </div>
         )
     }
