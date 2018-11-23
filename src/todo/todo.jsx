97b2a8d45a613d6240f3c0a5699ab565200677c9
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-23 17:07:29 -0200

    Preparando o componente Todo para o evento de adicionar tarefas.

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this)
+    }
 
     render() {
         return (
             <div>
                 <PageHeader name={'Tarefas'} small={'Cadastro'} />
-                <TodoForm />
+                <TodoForm  description={this.state.description}
+                    handleAdd={this.handleAdd}/>
+                <TodoList />
             </div>
         )
+    }
+
+    handleAdd() {
+
     }
 
 }
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
 
-    <div>
-        <h1>Form</h1>
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10' >
+            <input id='description' className={'form-control'}
+                placeholder={'Adicione uma tarefa'} value={props.description}/>
+        </Grid>
+
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}/>
+        </Grid>
     </div>
 
 )
