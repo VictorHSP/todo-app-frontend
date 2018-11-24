d36bdbc3f9af0d9d70d4dfa12aa76a29ff9c7655
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-24 01:07:07 -0200

    Adicionando os eventos para alterar uma tarefa.

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -16,6 +16,8 @@ export default class Todo extends Component {
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
 
         this.refresh()
     }
@@ -28,7 +30,9 @@ export default class Todo extends Component {
                     handleAdd={this.handleAdd}
                     handleChange={this.handleChange}/>
                 <TodoList list={ this.state.list }
-                          handleRemove={this.handleRemove} />
+                          handleRemove={this.handleRemove}
+                          handleMarkAsDone={this.handleMarkAsDone}
+                          handleMarkAsPending={this.handleMarkAsPending}/>
             </div>
         )
     }
@@ -50,6 +54,16 @@ export default class Todo extends Component {
 
     handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
             .then(resp => this.refresh())
     }
 
