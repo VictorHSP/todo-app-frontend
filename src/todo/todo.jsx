df3d097a73ed081ff87d10cbbde970a99be5a4f2
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-23 23:53:13 -0200

    Chamando a API para adicionar e deletar tarefas.

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+import axios from 'axios'
+
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
 
     constructor(props) {
         super(props)
         this.state = { description: '', list: [] }
+
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
     }
 
     render() {
@@ -16,14 +25,32 @@ export default class Todo extends Component {
             <div>
                 <PageHeader name={'Tarefas'} small={'Cadastro'} />
                 <TodoForm  description={this.state.description}
-                    handleAdd={this.handleAdd}/>
-                <TodoList />
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}/>
+                <TodoList list={ this.state.list }
+                          handleRemove={this.handleRemove} />
             </div>
         )
     }
 
-    handleAdd() {
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
 
+    handleAdd() {
+        const description = this.state.description;
+        axios.post(URL, { description })
+            .then(resp => this.refresh())
+    }
+
+    handleChange(event) {
+        this.setState({ ...this.state, description: event.target.value })
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
 }
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -7,7 +7,8 @@ export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10' >
             <input id='description' className={'form-control'}
-                placeholder={'Adicione uma tarefa'} value={props.description}/>
+                placeholder={'Adicione uma tarefa'} value={props.description}
+                onChange={props.handleChange}/>
         </Grid>
 
         <Grid cols='12 3 2'>
