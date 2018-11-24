c85869fc736c56ef1c2326f33ceabc990d250682
VictorHSP <victor.hugo.s@hotmail.com>
2018-11-24 09:47:09 -0200

    Finalizando o projeto como eventos ao clicar 'Enter', 'Shift + Enter' e 'Escape'

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -4,12 +4,21 @@ import IconButton from '../template/iconButton'
 
 export default props => {
 
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
     return (
         <div role='form' className='todoForm'>
             <Grid cols='12 9 10'>
                 <input id='description' className={'form-control'}
                        placeholder={'Adicione uma tarefa'} value={props.description}
-                       onChange={props.handleChange}/>
+                       onChange={props.handleChange}
+                        onKeyUp={keyHandler}/>
             </Grid>
 
             <Grid cols='12 3 2'>
